f2d7411373d3c889c0134de5be315180c88a96bd
Gadhilinga <gadilingappa2002@gmail.com>
2026-07-17 06:30:14 +0530

    changes

--- main.py
+++ main.py
@@ -85,6 +85,6 @@ def get_user(user_id: int, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id == user_id).first()
 
     if not user:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(status_code=404, detail="User not found.")
 
     return user
