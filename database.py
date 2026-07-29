4b90efa6446bfbaa5482a80527d358ff64f79945
Tejasri016 <tejasrisomarouthu16@gmail.com>
2026-07-29 22:50:45 +0530

    env update

--- database.py
+++ database.py
@@ -1,12 +1,12 @@
 import os
 
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql+psycopg2://postgres:Gadhi%402002@db.iktnhkmwfjtbumeiomjc.supabase.co:5432/postgres?sslmode=require",
-)
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
