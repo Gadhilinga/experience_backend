55facb6b64f8bf417c7640100972fafe09211bd3
Tejasri016 <tejasrisomarouthu16@gmail.com>
2026-07-30 18:50:33 +0530

    database url change

--- database.py
+++ database.py
@@ -1,5 +1,4 @@
 import os
-
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
@@ -7,6 +6,8 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+print("DATABASE_URL:", repr(DATABASE_URL))
 
 engine = create_engine(DATABASE_URL)
 
@@ -17,7 +18,6 @@ SessionLocal = sessionmaker(
 )
 
 Base = declarative_base()
-
 
 def get_db():
     db = SessionLocal()
